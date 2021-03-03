e92c0bfd832b95590513f27c695616e9aa32378a
Moshiur Rahman <rmoshiur705@gmail.com>
2021-03-04 00:15:21 +0600

    isString Added

---
+++ isString.js
@@ -0,0 +1,9 @@
+/**
+ * isString(value) check the value if string
+ * @param { value } any type of value
+ * @returns { boolean } returns true if String otherwise False
+ * 
+ */
+export default function isString(value) {
+    return typeof value === "string";
+}
